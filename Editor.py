63d245f3d977e7d9b0d9b571a11827a8fb0652b9
Nythepegasus <nythepegasus84@gmail.com>
2021-06-21 03:26:55 -0400

    Massive overhaul of the base editor, still a ton to do (in TODO.md)

--- Editor.py
+++ Editor.py
@@ -1,11 +1,12 @@
 import json
 import tempfile
-#  from pprint import pprint
-from pdf2image import convert_from_path
-from pathlib import Path
-from PIL.ImageTk import PhotoImage, Image
 import tkinter as tk
 from tkinter import ttk, StringVar, filedialog, messagebox
+from pdf2image import convert_from_path as cfp
+from pdf2image.exceptions import PDFPageCountError
+from pathlib import Path
+from PIL.ImageTk import PhotoImage, Image
+from ui import Canvas, TouchRect, ExtendedRect, TouchRegion
 
 
 class TypeChooser(tk.Toplevel):
@@ -47,99 +48,159 @@ class TypeChooser(tk.Toplevel):
             self.destroy()
 
 
-class Info(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.parent = parent
-        self._create_widgets()
-
-    def _create_widgets(self):
-        self.inputs = StringVar()
-        self.frame_x = StringVar()
-        self.frame_y = StringVar()
-        self.width = StringVar()
-        self.height = StringVar()
-        ttk.Label(self, text="Inputs: ").grid(row=0)
-        ttk.Label(self, textvariable=self.inputs).grid(row=0, column=1)
-        ttk.Label(self, text="Frame X: ").grid(row=1)
-        ttk.Label(self, textvariable=self.frame_x).grid(row=1, column=1)
-        ttk.Label(self, text="Frame Y: ").grid(row=2)
-        ttk.Label(self, textvariable=self.frame_y).grid(row=2, column=1)
-        ttk.Label(self, text="Width: ").grid(row=3)
-        ttk.Label(self, textvariable=self.width).grid(row=3, column=1)
-        ttk.Label(self, text="Height: ").grid(row=4)
-        ttk.Label(self, textvariable=self.height).grid(row=4, column=1)
-
-
 class Editor(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.sel_img: TouchRect = None
+        self.bind("<Button-1>", self.__info)
+        self.bind("<Shift-1>", self.__info)
+        self.bind("<Button-2>", self.__redraw)
 
     def ready(self):
         config = json.load((self.results[0] / "info.json").open())
         self.title(f"{config['name']} | {self.results[2].capitalize()}")
         self.mapping = config['representations']['iphone'][self.results[1]][self.results[2]]
-        self._create_widgets()
-        self.bind("<Button-1>", self.collision)
+        self.__create_widgets()
 
-    def _create_widgets(self):
+    def __create_widgets(self):
         with tempfile.TemporaryDirectory() as tmpdir:
             tmpdir = Path(tmpdir)
-            self.canvas = tk.Canvas(self, width=self.mapping["mappingSize"]["width"],
+            try:
+                try:
+                    cfp(self.results[0] / self.mapping["assets"]["resizable"],
+                        size=(self.mapping["mappingSize"]["width"],
+                        self.mapping["mappingSize"]["height"]))[0].save(tmpdir / "temp.png", "PNG")
+                except KeyError:
+                    cfp(self.results[0] / self.mapping["assets"]["large"],
+                        size=(self.mapping["mappingSize"]["width"],
+                              self.mapping["mappingSize"]["height"]))[0].save(tmpdir / "temp.png", "PNG")
+                background_file = Image.open(tmpdir / "temp.png")
+            except PDFPageCountError:  # Assume it's already a png
+                background_file = Image.open(self.results[0] / self.mapping["assets"]["large"])
+
+            print(background_file.filename)
+            background_file = background_file.resize(size=(self.mapping["mappingSize"]["width"],
+                                                           self.mapping["mappingSize"]["height"]))
+            self.canv_image = PhotoImage(background_file)
+            self.canvas = Canvas(self, width=self.mapping["mappingSize"]["width"],
                                     height=self.mapping["mappingSize"]["height"])
-            convert_from_path(self.results[0] / self.mapping["assets"]["resizable"],
-                              size=(self.mapping["mappingSize"]["width"],
-                                    self.mapping["mappingSize"]["height"]))[0].save(tmpdir / "temp.png", "PNG")
-            print(str(tmpdir / "temp.png"))
-            image = PhotoImage(Image.open(str(tmpdir / "temp.png")))
-            self.canvas.background = image  # TKinter why??
-            self.canvas.create_image(0, 0, image=image, anchor="nw")
-        self.canvas.ext_boxes = []
-        self.canvas.inp_boxes = []
-        for item in self.mapping["items"]:
-            dext = self.mapping['extendedEdges']
-            iext = item['extendedEdges'] if "extendedEdges" in item.keys() else dext
-            iframe = item['frame']
-            left = (iext['left'] if "left" in iext.keys() else dext['left'])
-            right = (iext['right'] if "right" in iext.keys() else dext['right'])
-            top = (iext['top'] if "top" in iext.keys() else dext['top'])
-            bottom = (iext['bottom'] if "bottom" in iext.keys() else dext['bottom'])
-            ex_x = iframe['x'] - left
-            ex_y = iframe['y'] - top
-            ex_w = iframe['x'] + iframe['width'] + right
-            ex_h = iframe['y'] + iframe['height'] + bottom
-            self.canvas.ext_boxes.append([ex_x, ex_y, ex_w, ex_h])
-            self.canvas.create_rectangle(ex_x, ex_y, ex_w, ex_h, outline="#f51637",
-                                         width=3, fill="", stipple="gray25")
-            self.canvas.create_rectangle(iframe['x'], iframe['y'],
-                                         iframe['x'] + iframe['width'], iframe['y'] + iframe['height'],
-                                         outline='#594ced', width=3, fill="", stipple="gray25")
+            self.canvas.background = self.canv_image  # tkinter why??
+            self.canvas.create_image(0, 0, image=self.canv_image, anchor="nw")
+            self.canvas.pack()
 
-            self.canvas.inp_boxes.append([iframe['x'], iframe['y'],
-                                          iframe['x'] + iframe['width'], iframe['y'] + iframe['height']])
-        self.canvas.pack()
+        dext = self.mapping['extendedEdges']
+        for item in self.mapping['items']:
+            iext = item['extendedEdges'] if "extendedEdges" in item.keys() else dext.copy()
+            for i in dext.keys():
+                if i not in iext.keys():
+                    iext[i] = dext[i]
+            iframe = item["frame"]
 
-    def collision(self, pos):
-        print(f"X: {pos.x}\nY: {pos.y}")
-        for inp in self.canvas.inp_boxes:
-            if inp[0] < pos.x < inp[2]:
-                if inp[1] < pos.y < inp[3]:
-                    sel = self.canvas.find_closest(inp[0], inp[1])
-                    break
+            width_add = (0 if 'left' not in iext.keys() else iext['left']) + \
+                        (0 if 'right' not in iext.keys() else iext['right']) + \
+                        iframe['width']
+            height_add = (0 if 'top' not in iext.keys() else iext['top']) + \
+                         (0 if 'bottom' not in iext.keys() else iext['bottom']) + \
+                         iframe['height']
+
+            ex_x = iframe['x'] - (0 if 'left' not in iext.keys() else iext['left'])
+            ex_y = iframe['y'] - (0 if 'top' not in iext.keys() else iext['top'])
+
+            rectImg = TouchRect("#0000ff80", [iframe['width'], iframe['height']], [iframe['x'], iframe['y']])
+
+            rectImg1 = ExtendedRect("#ff000080", [width_add, height_add], [ex_x, ex_y])
+            region = TouchRegion(self.canvas, rectImg, rectImg1, item['inputs'])
+            region.create()
+            self.canvas.regions.append(region)
+
+    def __redraw(self, _):
+        self.canvas.delete("all")
+        self.canvas.background = self.canv_image
+        self.canvas.create_image(0, 0, image=self.canv_image, anchor="nw")
+        for region in self.canvas.regions:
+            region.create()
+
+    def __sel(self):
+        self.bind("+", self.__increase)
+        self.bind("=", self.__increase)
+        self.bind("<plusminus>", self.__increase)
+        self.bind("<notequal>", self.__increase)
+        self.bind("<endash>", self.__decrease)
+        self.bind("<emdash>", self.__decrease)
+        self.bind("-", self.__decrease)
+        self.bind("_", self.__decrease)
+        self.bind("<Left>", self.__move)
+        self.bind("<Right>", self.__move)
+        self.bind("<Up>", self.__move)
+        self.bind("<Down>", self.__move)
+
+    def __info(self, event):
+        if self.canvas.find_withtag("sel"):
+            self.__redraw(None)
+        sel = self.canvas.find_closest(event.x, event.y)
+        print(sel)
+        print(len(self.canvas.images))
+        if event.state == 1:
+            overlapped = self.canvas.find_overlapping(event.x, event.y, event.x, event.y)
+            if len(overlapped) > 2:
+                sel = (overlapped[1],)
+        if sel[0] == 1:
+            return
+        sel_img = [i for i in self.canvas.images if i.name == self.canvas.itemcget(sel, "image")][0]
+        self.sel_img = sel_img
+        print(self.sel_img.parent.input)
+        self.canvas.addtag_withtag("sel", sel)
+        self.__sel()
+
+    def __decrease(self, event):
+        x, y = 0, 0
+        if event.char in ["—", "–"]:
+            y -= 1 if event.char == "–" else 5
+            if self.sel_img.height() + y <= 0:
+                y = 0
         else:
-            sel = None
-        if sel is None:
-            for ext in self.canvas.ext_boxes:
-                if ext[0] < pos.x < ext[2]:
-                    if ext[1] < pos.y < ext[3]:
-                        sel = self.canvas.find_closest(ext[0]+6, ext[1])
-                        if len(self.canvas.find_below(sel)) == 1:
-                            sel = self.canvas.find_below(sel)
-                        break
+            x -= 1 if event.char == "-" else 5
+            if self.sel_img.width() + x <= 0:
+                x = 0
+        if isinstance(self.sel_img, ExtendedRect):
+            self.sel_img.parent.update(w=x, h=y, touch=False)
+        else:
+            self.sel_img.parent.update(w=x, h=y, touch=True)
+
+    def __increase(self, event):
+        x, y = 0, 0
+        if event.char in ["±", "≠"]:
+            y += 1 if event.char == "≠" else 5
+        else:
+            x += 1 if event.char == "=" else 5
+        if isinstance(self.sel_img, ExtendedRect):
+            self.sel_img.parent.update(w=x, h=y, touch=False)
+        else:
+            self.sel_img.parent.update(w=x, h=y, touch=True)
+
+    def __move(self, event):
+        if self.canvas.find_withtag("sel"):
+            x, y = 0, 0
+            if event.keysym in ["Left", "Right"]:
+                x = 1 if event.keysym == "Right" else -1
+                if event.state == 112:
+                    x = 5 if event.keysym == "Right" else -5
+                elif event.state == 97:
+                    x = 10 if event.keysym == "Right" else -10
+                elif event.state == 113:
+                    x = 20 if event.keysym == "Right" else -20
             else:
-                sel = None
-        if sel is not None:
-            self.canvas.itemconfig(sel, outline="#00ff00")
+                y = 1 if event.keysym == "Down" else -1
+                if event.state == 112:
+                    y = 5 if event.keysym == "Down" else -5
+                elif event.state == 97:
+                    y = 10 if event.keysym == "Down" else -10
+                elif event.state == 113:
+                    y = 20 if event.keysym == "Down" else -20
+            if isinstance(self.sel_img, ExtendedRect):
+                self.sel_img.parent.update(x, y, False)
+            else:
+                self.sel_img.parent.move(x, y)
 
 
 if __name__ == '__main__':
