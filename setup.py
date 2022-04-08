5a9a6883272bad01f5a12969ec7d995ca0fe5143
Nythepegasus <nythepegasus84@gmail.com>
2022-04-08 10:03:49 -0400

    Add setup.py with steps to build macOS applications

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import os
+import pwd
+import shutil
+import platform
+from setuptools import setup
+
+if platform.system() == "Darwin":
+    if os.getuid() != 0:
+        raise PermissionError("Must be superuser to build for Mac.")
+
+    APP = ["Editor.py"]
+    DATA_FILES = []
+    OPTIONS = {
+        "packages": ["PIL", "pdf2image", "yaml"],
+        "resources": [],
+        "redirect_stdout_to_asl": True,
+    }
+
+    if "arm" in platform.machine():
+        OPTIONS["resources"].append("/opt/homebrew/Cellar/poppler/22.02.0/bin")
+    elif "x86" in platform.machine():
+        OPTIONS["resources"].append("/usr/local/homebrew/Cellar/poppler/22.02.0/bin")
+    else:
+        raise SystemError(f"Unknown arch: {platform.machine()}")
+
+    setup(
+        app=APP,
+        data_files=DATA_FILES,
+        py_modules=[],
+        options={'py2app': OPTIONS},
+        setup_requires=['py2app'],
+    )
+
+    os.chdir("dist/Editor.app/Contents/Resources/")
+    os.symlink("../Frameworks", "Frameworks", target_is_directory=True)
+    os.chown("../../", pwd.getpwnam(os.getlogin()).pw_uid, pwd.getpwnam(os.getlogin()).pw_gid)
+    if "arm" in platform.machine():
+        shutil.copytree("/opt/homebrew/Cellar/poppler/22.02.0/lib/", "../Frameworks", dirs_exist_ok=True)
+    else:
+        shutil.copytree("/usr/local/homebrew/Cellar/poppler/22.02.0/lib/", "../Frameworks", dirs_exist_ok=True)
+
+    print("Copied libraries over to the app. Test before shipping!")
